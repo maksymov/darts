0e362a72d8afbd2278b673caa49de6b30975f227
Владислав Максимов <mail@vlad85.ru>
2025-08-04 15:22:12 +0300

    commit

--- static/sw.js
+++ static/sw.js
@@ -1,18 +1,36 @@
 // static/sw.js
+const CACHE_NAME = 'darts-cache-v2'; // ← Обновлённый кэш-ключ
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/static/jquery-3.7.0.min.js',
+  '/static/fontawesome/all.min.js',
+  '/static/bootstrap/bootstrap.bundle.min.js',
+  '/static/script.js',
+  '/static/style.css',
+  '/static/manifest.json',
+  '/static/img/icon-192x192.png',
+  '/static/img/icon-512x512.png'
+];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('darts-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/static/jquery-3.7.0.min.js',
-        '/static/fontawesome/all.min.js',
-        '/static/bootstrap/bootstrap.bundle.min.js',
-        '/static/script.js',
-        '/static/style.css',
-        '/static/img/icon-192x192.png',
-        '/static/img/icon-512x512.png'
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // ← Удаляем старые кэши
+          }
+        })
+      );
     })
   );
 });
