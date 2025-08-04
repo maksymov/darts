aadeae07ac896f3bd6e8858b929cdb5111f699ef
Владислав Максимов <mail@vlad85.ru>
2025-08-04 16:52:59 +0300

    commit

--- static/sw.js
+++ static/sw.js
@@ -1,36 +1,23 @@
 // static/sw.js
-const CACHE_NAME = 'darts-cache-v2'; // ← Обновлённый кэш-ключ
+const CACHE_NAME = 'darts-cache-v2';
 const urlsToCache = [
   '/',
-  '/index.html',
   '/static/jquery-3.7.0.min.js',
   '/static/fontawesome/all.min.js',
   '/static/bootstrap/bootstrap.bundle.min.js',
   '/static/script.js',
   '/static/style.css',
+  '/static/bootstrap/scss/bootstrap.css',
   '/static/manifest.json',
-  '/static/img/icon-192x192.png',
-  '/static/img/icon-512x512.png'
+  '/static/img/apple-touch-icon.png',
+  '/static/img/write.png',
+  '/static/img/donate.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName); // ← Удаляем старые кэши
-          }
-        })
-      );
     })
   );
 });
